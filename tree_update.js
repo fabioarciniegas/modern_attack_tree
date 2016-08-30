4cfee6e11277099ffafe781f4d965dcb6ce801ce
fabioa <fab.arciniegas@gmail.com>
2016-08-30 11:19:39 +0800

    proper animation of nodes

--- tree_update.js
+++ tree_update.js
@@ -3,34 +3,31 @@
 
 /* TODO: add contents from the codemirror, in a smart way */
 function update() {
-        /* based on http://bl.ocks.org/mbostock/999346 */
-        if (toggleCollapse = !toggleCollapse)
-            $("#tree-container>svg").css('background', '');
-        else
-            $("#tree-container>svg").css('background', 'white');
+    /* based on http://bl.ocks.org/mbostock/999346 */
+    if (toggleCollapse = !toggleCollapse)
+        $("#tree-container>svg").css('background', '');
+    else
+        $("#tree-container>svg").css('background', 'white');
 
-        /** Group containing the nodes and links **/
-        svg = d3.select("#tree-container>svg>g");
+    /** Group containing the nodes and links **/
+    svg = d3.select("#tree-container>svg>g");
 
-        var node = svg.selectAll(".node"),
-            link = svg.selectAll(".link");
-
+    var node = svg.selectAll(".node"),
+        link = svg.selectAll(".link");
 
 	/ * create a node, assign it at random to a parent */
     var n = {id: nodes.length, "name": ""+ nodes.length},
             p = nodes[Math.random() * nodes.length | 0];
 	
-        if (p.children) p.children.push(n); else p.children = [n];
+    if (p.children) p.children.push(n); else p.children = [n];
 
-    /* very confusing because calculated outside of the function */
-    
-	nodes.push(n);
-	/* no need to add a link explicity  to the model.
-	   But now reconstruct the VISUAL nodes using tree.nodes( */
-
+    /* very confusing because nodes was calculated outside of the function */
+    nodes.push(n);
+    /* no need to add a link explicity  to the model?
+       But now reconstruct the VISUAL nodes using tree.nodes( */
 
     // Recompute the layout and data join.
-    // Notice that the root tree DATA has been automagically updated by nodes.push
+    // Notice that the root tree DATA has been updated by nodes.push
     // resulting on a very convenient root call
     node = node.data(tree.nodes(root), function (d) {
         return d.id;
@@ -47,19 +44,20 @@ function update() {
         .attr("cx", function (d) {
 	    // parent is a apparently a propertiy updated by the
 	    // layout routine
-                    return d.x;
+                    return d.parent.x;
                 })
                 .attr("cy", function (d) {
-                    return d.y;
+                    return d.parent.y;
                 });
 
+    // TODO: start the text transparent and transition to the final color
     nodeEnter.append("text")
         .attr("class", "text")
 	.attr("x", function(d) {
-            return d.x;
+            return d.parent.x;
 	})
 	.attr("y", function(d) {
-            return d.y;
+            return d.parent.y;
 	})
 	.style("fill","red")
 	.text(function(d) {
@@ -68,23 +66,24 @@ function update() {
 
 
     // Add entering links in the parent’s old position.
+    /*
     link.enter().insert("path", ".node")
         .attr("class", "link")
         .attr("d", function (d) {
-            var o = {x: d.source.px, y: d.source.py};
-            return diagonal({source: o, target: o});
+            var o = {x: d.px, y: d.py};
+	    var p = {x: d.x, y: d.y};
+            return diagonal({source: p, target: o});
         });
-
+*/
     // Not quite sure what happens to removed nodes
 
     // Transition nodes and links to their new positions.
 
     var t = svg.transition()
-        .duration(10);
+        .duration(750);
 
-
-    t.selectAll(".link")
-        .attr("d", diagonal);
+//    t.selectAll(".link")
+//        .attr("d", diagonal);
 
     t.selectAll(".node")
         .attr("cx", function (d) {
